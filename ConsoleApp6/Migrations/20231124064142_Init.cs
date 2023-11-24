4fbea8c292e61799ae44a752e04fb5c7ebdd1c87
PC <PC@C21-17>
2023-11-24 10:51:59 +0400

    Add project files.

---
+++ ConsoleApp6/Migrations/20231124064142_Init.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConsoleApp6.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Lectures",
+                columns: table => new
+                {
+                    LectureId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Capacity = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lectures", x => x.LectureId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    EnrollmentDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LectureStudent",
+                columns: table => new
+                {
+                    LecturesLectureId = table.Column<int>(type: "INTEGER", nullable: false),
+                    StudentsStudentId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LectureStudent", x => new { x.LecturesLectureId, x.StudentsStudentId });
+                    table.ForeignKey(
+                        name: "FK_LectureStudent_Lectures_LecturesLectureId",
+                        column: x => x.LecturesLectureId,
+                        principalTable: "Lectures",
+                        principalColumn: "LectureId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LectureStudent_Students_StudentsStudentId",
+                        column: x => x.StudentsStudentId,
+                        principalTable: "Students",
+                        principalColumn: "StudentId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LectureStudent_StudentsStudentId",
+                table: "LectureStudent",
+                column: "StudentsStudentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LectureStudent");
+
+            migrationBuilder.DropTable(
+                name: "Lectures");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+public class Student
+{
+    public int StudentId { get; set; }
+    public string Name { get; set; }
+    public DateTime EnrollmentDate { get; set; }
+    public List<Lecture> Lectures { get; set; } = new List<Lecture>();
+}
+
+public class Lecture
+{
+    public int LectureId { get; set; }
+    public string Title { get; set; }
+    public DateTime Date { get; set; }
+    public int Capacity { get; set; }
+    public List<Student> Students { get; set; } = new List<Student>();
+}
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Lecture> Lectures { get; set; }
+
+    public string DbPath { get; }
+
+    public AppDbContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "student.db");
+    }
+
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+      =>  options.UseSqlite($"Data Source={DbPath}"); 
+   
+}
+
+
+public class LectureRepository
+{
+    private readonly AppDbContext _context;
+
+    public LectureRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public void AddLecture(Lecture lecture)
+    {
+        _context.Lectures.Add(lecture);
+        _context.SaveChanges();
+    }
+
+    public Lecture findLecture(int lectureId)
+    {
+        return _context.Lectures.Find(lectureId);
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        using (var context = new AppDbContext())
+        {
+            var lectureRepo = new LectureRepository(context);
+
+            var lecture = new Lecture
+            {
+                Title = ".NET Technologies",
+                Date = DateTime.Now,
+                Capacity = 30
+            };
+
+            var student1 = new Student { Name = "Ani", EnrollmentDate = DateTime.Now };
+            var student2 = new Student { Name = "Giorgi", EnrollmentDate = DateTime.Now };
+
+            lecture.Students.Add(student1);
+            lecture.Students.Add(student2);
+
+            lectureRepo.AddLecture(lecture);
+
+            var storedLecture = lectureRepo.findLecture(lecture.LectureId);
+            if (storedLecture != null)
+            {
+                Console.WriteLine($"Lecture: {storedLecture.Title}, Date: {storedLecture.Date}");
+                foreach (var student in storedLecture.Students)
+                {
+                    Console.WriteLine($"Student: {student.Name}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No lecture found.");
+            }
+        }
+    }
+}
